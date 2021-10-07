6610d13943e0e6cfda7f4be25300ac766a2c0ca9
jmrz97 <91626604+jmrz97@users.noreply.github.com>
2021-10-07 17:33:40 -0600

    Add files via upload

---
+++ eliminar_editorial.php
@@ -0,0 +1,33 @@
+<?php
+    if (isset($_GET['codigo'])) {
+        include 'conexion.php';
+        $conn = OpenCon();
+           
+        // Verificamos la conexión
+        if ($conn == null) {
+            die("No se pudo conectar a la base de datos: ");
+            header('Location: /inventario_pdo/editorial/listar_editorial.php?result=0');
+        } 
+        
+        $sql = 'DELETE FROM editoriales WHERE codigoEditorial = ?';
+
+        $sth = $conn->prepare($sql);
+
+        $sth->execute(array($_GET['codigo']));
+
+        $count = $sth ->rowCount();
+
+        if($count > 0){
+            header('Location: /inventario_pdo/editorial/listar_editoriales.php?result=1');
+            exit();
+        }
+        else{
+            header('Location: /inventario_pdo/editorial/listar_editoriales.php?result=0');
+            exit();
+        }
+
+        CloseCon($conn);
+    } else {
+        header('Location: /inventario_pdo/editorial/listar_editoriales.php?result=0');
+    }
+?>
